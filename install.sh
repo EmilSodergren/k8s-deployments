a6f3b7a4d38ca74aa821c74f7b19539ca743aa41
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2023-02-18 15:06:37 +0100

    Fixed up the install script

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_SYMLINK="force" \
-  INSTALL_K3S_SKIP_ENABLE="true" sh - -- --disable servicelb
-kubectl apply _f coredns/coredns-cm.yaml
+curl -sfL https://get.k3s.io | \
+  K3S_KUBECONFIG_MODE="644" \
+  INSTALL_K3S_SYMLINK="force" \
+  INSTALL_K3S_EXEC="--disable servicelb" \
+  sh -
+kubectl apply -f coredns/coredns-cm.yaml
 sudo systemctl start k3s
+echo "Edit the configmap:"
+echo "    kubectl -n kube-system edit cm coredns -o yaml"
+echo "Change line 21, . /etc/resolve.conf to:   import /etc/coredns/custom/*.forward"
+echo "    kubectl -n kube-system delete po -l k8s-app=kube-dns"
