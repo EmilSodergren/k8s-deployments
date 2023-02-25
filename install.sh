7430b4095aa4cd261994c8bf58952d201cb19617
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2023-02-25 19:35:56 +0100

    Automatically setup coredns to use custom forwarding
    Forward to quad9

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash -e
 
 curl -sfL https://get.k3s.io | \
-  K3S_KUBECONFIG_MODE="644" \
   INSTALL_K3S_SYMLINK="force" \
-  INSTALL_K3S_EXEC="--disable servicelb" \
+  INSTALL_K3S_EXEC="--disable servicelb,traefik --kube-proxy-arg proxy-mode=ipvs --kube-proxy-arg ipvs-strict-arp" \
   sh -
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/k3s.yaml
 kubectl apply -f coredns/coredns-cm.yaml
-sudo systemctl start k3s
-echo "Edit the configmap:"
-echo "    kubectl -n kube-system edit cm coredns -o yaml"
-echo "Change line 21, . /etc/resolve.conf to:   import /etc/coredns/custom/*.forward"
-echo "    kubectl -n kube-system delete po -l k8s-app=kube-dns"
+kubectl -n kube-system get cm coredns -o yaml | sed 's#forward . /etc/resolv.conf#import /etc/coredns/custom/*.forward#' | kubectl replace -f -
+kubectl -n kube-system delete po -l k8s-app=kube-dns
