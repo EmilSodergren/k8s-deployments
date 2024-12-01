a65a72adfb810e522c47922631803b28216a558f
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2024-12-01 14:42:07 +0100

    Added install dns only mode in install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,43 @@
 #!/bin/bash -e
 
-if [ "$1" == "master" ]; then
+function install_k3s {
   curl -sfL https://get.k3s.io | \
     INSTALL_K3S_SYMLINK="force" \
     INSTALL_K3S_EXEC="--disable servicelb,traefik --kube-proxy-arg proxy-mode=ipvs --kube-proxy-arg ipvs-strict-arp --write-kubeconfig-mode=644" \
     sh -
   sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/k3s.yaml
+}
+
+function setup_dns {
   kubectl apply -f coredns/coredns-cm.yaml
   sleep 3
   kubectl -n kube-system get cm coredns -o yaml | sed 's#forward . /etc/resolv.conf#import /etc/coredns/custom/*.forward#' | kubectl replace -f -
   sleep 3
   kubectl -n kube-system delete po -l k8s-app=kube-dns
   sleep 3
+}
+
+function print_token {
   TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
   echo TOKEN = "$TOKEN"
+}
 
-elif [ "$1" == "agent" ]; then
+function setup_agent {
   if [ -z "$2" ]; then
     echo "ERROR: Need token as argument 2 to connect the agent"
     exit 1
   fi
   curl -sfL https://get.k3s.io | K3S_URL=https://pi.home:6443 K3S_TOKEN="$2" sh -
+}
 
+if [[ "$1" == "master" ]]; then
+  install_k3s
+  setup_dns
+  print_token
+elif [[ "$1" == "agent" ]]; then
+  setup_agent
+elif [[ "$1" == "dns_only" ]]; then
+  setup_dns
 else
-  echo ERROR: Arguments needs to be \'master\' or \'agent "TOKEN"\'
+  echo ERROR: Arguments needs to be \'master\' or \'agent "TOKEN"\' or \'dns_only\'
 fi
